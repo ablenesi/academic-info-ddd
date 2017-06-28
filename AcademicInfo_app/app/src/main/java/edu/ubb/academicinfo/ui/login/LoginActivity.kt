da6f1414d253a55207586f7910e17ef4ce61659c
Attila Blénesi <battila@halcyonmobile.com>
2017-06-28 19:07:19 +0300

    Add Authentication service - Implement networking related classes

--- AcademicInfo_app/app/src/main/java/edu/ubb/academicinfo/ui/login/LoginActivity.kt
+++ AcademicInfo_app/app/src/main/java/edu/ubb/academicinfo/ui/login/LoginActivity.kt
@@ -1,18 +1,31 @@
 package edu.ubb.academicinfo.ui.login
 
+import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import edu.ubb.academicinfo.R
 import edu.ubb.academicinfo.databinding.ActivityLoginBinding
+import edu.ubb.academicinfo.model.User
+import edu.ubb.academicinfo.ui.home.HomeActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginViewModel.EventHandler {
 
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        binding.viewModel = LoginViewModel()
+        binding.viewModel = LoginViewModel(this)
     }
+
+    override fun showError(error: String) {
+        Snackbar.make(binding.root, error, Snackbar.LENGTH_LONG)
+    }
+
+    override fun loginSuccess(user: User?) {
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+
 }
--- AcademicInfo_app/app/src/main/java/edu/ubb/academicinfo/ui/login/LoginViewModel.kt
+++ AcademicInfo_app/app/src/main/java/edu/ubb/academicinfo/ui/login/LoginViewModel.kt
@@ -1,16 +1,46 @@
 package edu.ubb.academicinfo.ui.login
 
 import android.databinding.ObservableBoolean
+import edu.ubb.academicinfo.AcademicInfoApp
+import edu.ubb.academicinfo.api.AuthenticationService
+import edu.ubb.academicinfo.api.LoginDTO
+import edu.ubb.academicinfo.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
 
-class LoginViewModel {
+class LoginViewModel(val handler: LoginViewModel.EventHandler) {
+
+    interface EventHandler {
+        fun showError(error: String)
+        fun loginSuccess(user: User?)
+    }
+
+    @Inject
+    lateinit var authService: AuthenticationService
+
+    init {
+        AcademicInfoApp.appComponent.inject(this)
+    }
 
     var email = ""
     var password = ""
+
     var loading: ObservableBoolean = ObservableBoolean(false)
 
     fun login() {
         loading.set(true)
-        print(email)
-        // TODO Implement Log In request
+        authService.login(LoginDTO(email, password)).enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>?, response: Response<User>?) {
+                handler.loginSuccess(response?.body())
+            }
+
+            override fun onFailure(call: Call<User>?, t: Throwable?) {
+                handler.showError(t?.message ?: "Something went wrong")
+                loading.set(false)
+            }
+
+        })
     }
 }
